292f05c99d029067db4e2661dd9389fc5164b8db
Anthony Damtsis <adamtsis@dius.com.au>
2015-02-18 13:59:39 +1100

    Explicit Accept header of 'application/json' added

--- myapp.rb
+++ myapp.rb
@@ -6,7 +6,6 @@ require 'json'
 get '/:number' do
     resp = RestClient.get "https://staging.api.telstra.com/v1/oauth/token?client_id=[client_id]&client_secret=[secret]&grant_type=client_credentials&scope=SMS"
     token = JSON.parse(resp)["access_token"]
-    header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json"}
+    header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json", "Accept" => "application/json"}
     RestClient.post "https://staging.api.telstra.com/v1/sms/messages", {to: params[:number], body: "Hi Frank, we are at the front door. This has been sent using the Telstra API >:)"}.to_json, header
-
 end
